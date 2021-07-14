a6b1d90b439ebd1512693a564374d4ffa25af147
Lucas Moreira <lucas-berto@hotmail.com>
2021-07-14 20:02:47 -0300

    [#1-> Inicialização do projeto] - Incremento
    *Adicionados arquivos de configuração.

---
+++ src/main/java/com/naberss/SocialMediaMockup/SocialMediaMockupApplication.java
@@ -0,0 +1,13 @@
+package com.naberss.SocialMediaMockup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaMockupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaMockupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naberss/SocialMediaMockup/SocialMediaMockupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naberss.SocialMediaMockup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaMockupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
