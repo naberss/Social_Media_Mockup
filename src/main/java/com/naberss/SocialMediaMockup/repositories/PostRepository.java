edd6ef0ca8a7480e08faeea640a8e49affbd0726
Lucas Moreira <lucas-berto@hotmail.com>
2021-07-17 13:01:49 -0300

    [#9->Incluídos métodos de pesquisa auxiliares] - Incremento

--- src/main/java/com/naberss/SocialMediaMockup/repositories/PostRepository.java
+++ src/main/java/com/naberss/SocialMediaMockup/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.naberss.SocialMediaMockup.repositories;
 
+import java.time.Instant;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -13,14 +14,20 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
 	@Query(value = "{'title': {$regex : ?0, $options: 'i'}}")
 	public List<Post> findbyTitle(String title);
-	
+
 	@Query(value = "{'body': {$regex : ?0, $options: 'i'}}")
 	public List<Post> findbyBody(String body);
-	
+
 	@Query(value = "{'author.id': {$regex : ?0, $options: 'i'}}")
 	public List<Post> findbyAuthorId(String id);
-	
+
 	@Query(value = "{'author.name': {$regex : ?0, $options: 'i'}}")
 	public List<Post> findbyAuthorName(String name);
-	
+
+	@Query(value = "{ $or:[ {'body': {$regex : ?0, $options: 'i'}}, {'title': {$regex : ?0, $options: 'i'}} ]}")
+	public List<Post> findbyText(String name);
+
+	@Query(value = "{ $and:[ {date:{$gte: ?0 }}, {date:{$lte: ?1 }} ]}")
+	public List<Post> findbyDateInterval(Instant DateI, Instant DateF);
+
 }
